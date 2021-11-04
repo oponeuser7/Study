55e4e786adaedb30f9b28044d5b845fb2ba3375b
lavgin7 <oponeuser7@gmail.com>
2021-11-04 17:06:24 +0900

    solve leetcode 980

--- Algorithm/leetcode/980.py
+++ Algorithm/leetcode/980.py
@@ -1,33 +1,32 @@
 class Solution:
-    def uniquePathsIII(self, grid):
-        stack = []
-        visited = set()
-        h = len(grid)
-        w = len(grid[0])
+    def uniquePathsIII(self, grid: List[List[int]]) -> int:
+        dx, dy = (1,-1,0,0), (0,0,1,-1)
+        m, n = len(grid), len(grid[0])
         count = 0
-        target = 0
-        for i in range(h):
-            for j in range(w):
-                if grid[i][j]==0 or grid[i][j]==2:
+        target = 1
+        goal = None
+        stack = []
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==1:
+                    stack.append((i,j,[(i,j)]))
+                elif grid[i][j]!=-1:
+                    if grid[i][j]==2:
+                        goal = (i,j)
                     target += 1
-                elif grid[i][j]==1:
-                    stack.append((j,i,0))
-        dx = (1,-1,0,0)
-        dy = (0,0,1,-1)
         while stack:
             temp = stack.pop()
-            x, y, v = temp[0], temp[1], temp[2]
-            if grid[y][x]==2:
-                if v==target:
+            y, x, visited = temp[0], temp[1], temp[2]
+            if y==goal[0] and x==goal[1]:
+                if len(visited)==target:
                     count += 1
-                    visited = set()
                 continue
-            visited.add((x,y))
             for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if 0<=nx<w and 0<=ny<h:
-                    if grid[ny][nx]!=-1 and (nx,ny) not in visited:
-                        stack.append((nx,ny,v+1))
+                nx, ny = x + dx[i], y + dy[i]
+                if 0<=nx<n and 0<=ny<m and (ny,nx) not in visited:
+                    if grid[ny][nx]!=-1:
+                        copied = visited[:]
+                        copied.append((ny,nx))
+                        stack.append((ny,nx,copied))
         return count
 
