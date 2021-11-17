44e1197212b6e1df2794827967d8e7dba285c47d
lavgin7 <oponeuser7@gmail.com>
2021-11-17 17:10:01 +0900

    algorithm study : LCA

--- 2021_fall_semester/알고리즘/10주차/연습/lca.py
+++ 2021_fall_semester/알고리즘/10주차/연습/lca.py
@@ -1,4 +1,5 @@
 from math import log2
+from collections import deque
 
 def generate_tree(tree, n):
     for _ in range(n-1):
@@ -6,11 +7,11 @@ def generate_tree(tree, n):
         tree[child].append(parent)
         tree[parent].append(child)
 
-def dfs(tree, parents, depth, n):
-    queue = [1]
+def bfs(tree, parents, depth, n):
+    queue = deque([1])
     visited = [False]*(n+1)
     while queue:
-        temp = queue.pop(0)
+        temp = queue.popleft()
         visited[temp] = True
         for child in tree[temp]:
             if not visited[child]:
@@ -19,23 +20,21 @@ def dfs(tree, parents, depth, n):
                 depth[child] = depth[temp]+1
 
 def compute_exp_parent(exp_parents, n):
-    logN = (int)(log2(n))
-    for i in range(1, n+1):
-        for j in range(1, logN):
-            exp_parents[i][j] = exp_parents[exp_parents[i][j-1]][j-1]
+    for i in range(1, logN):
+        for j in range(1, n+1):
+            exp_parents[j][i] = exp_parents[exp_parents[j][i-1]][i-1]
 
 def lca(exp_parents, depth, n, m):
-    logN = (int)(log2(n))
     for _ in range(m):
         a, b = map(int, input().split())
         if depth[a] > depth[b]:
             a, b = b, a
         level_diff = depth[b] - depth[a]
         for i in range(logN):
-            if level_diff & 1 << i:
+            if level_diff & (1 << i):
                 b = exp_parents[b][i]
         if a==b:
-            print(a)
+            print(b)
             continue
         for i in range(logN-1, -1, -1):
             if exp_parents[a][i] != exp_parents[b][i]:
@@ -44,14 +43,14 @@ def lca(exp_parents, depth, n, m):
         print(exp_parents[a][0])
 
 n = int(input())
+logN = int(log2(n)+1)
 tree = [[] for _ in range(n+1)]
 generate_tree(tree, n)
 parents = [0]*(n+1)
 depth = [0]*(n+1)
-dfs(tree, parents, depth, n)
-logN = (int)(log2(n))
+bfs(tree, parents, depth, n)
 exp_parents = [[0 for i in range(logN)] for j in range(n+1)]
-for i in range(1, n+1):
+for i in range(n+1):
     exp_parents[i][0] = parents[i]
 compute_exp_parent(exp_parents, n)
 m = int(input())
