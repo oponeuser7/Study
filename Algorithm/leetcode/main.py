f4be974c0236feb9c3ee567c83e99ede8d9e655a
lavgin7 <oponeuser7@gmail.com>
2021-11-19 19:06:31 +0900

    leetcode add testing function

---
+++ Algorithm/leetcode/main.py
@@ -0,0 +1,9 @@
+import _461
+
+def main():
+    solution = _461.Solution()
+    print("Here goes the answer: ")
+    print(solution.hammingDistance(1,32))
+    
+if __name__=="__main__":
+    main()
