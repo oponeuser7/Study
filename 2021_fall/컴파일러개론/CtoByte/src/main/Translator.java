0bc408e3a8175079652ce49b0b4c7e5b823c21cf
lavgin7 <oponeuser7@gmail.com>
2021-12-20 18:35:39 +0900

    compiler HW5-2 init

---
+++ 2021_fall/컴파일러개론/CtoByte/src/main/Translator.java
@@ -0,0 +1,18 @@
+package main;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import generated.*;
+
+public class Translator {
+
+	public static void main(String[] args) throws Exception {
+		CharStream codeCharStream = CharStreams.fromFileName("test.c");
+		MiniCLexer lexer = new MiniCLexer(codeCharStream);
+		CommonTokenStream tokens = new CommonTokenStream( lexer );
+		MiniCParser parser = new MiniCParser( tokens );
+		ParseTree tree = parser.program();
+		ParseTreeWalker walker = new ParseTreeWalker();
+		walker.walk(new BytecodeGenListener(), tree );
+	}
+}
