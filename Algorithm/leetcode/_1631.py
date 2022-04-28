a39523c9ec59e924e09bb5c480507928074d25ca
lavgin7 <oponeuser7@gmail.com>
2022-04-29 00:11:07 +0900

    leetcode daily solve 1631 final

--- Algorithm/leetcode/_1631.py
+++ Algorithm/leetcode/_1631.py
@@ -1,12 +1,12 @@
 class Solution:
-    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int: 
+        row = len(heights)
+        col = len(heights[0])
+        dy, dx = (0,0,1,-1), (1,-1,0,0)
         def bfs(k):
-            dy, dx = (0,0,1,-1), (1,-1,0,0)
-            row = len(heights)
-            col = len(heights[0])
             q = deque()
             visited = [[0 for i in range(col)] for j in range(row)]
-            q.append((y, x))
+            q.append((0, 0))
             while q:
                 temp = q.popleft()
                 y, x = temp[0], temp[1]
@@ -15,17 +15,17 @@ class Solution:
                     ny, nx = y+dy[i], x+dx[i]
                     if 0<=ny<row and 0<=nx<col and not visited[ny][nx]:
                         if abs(heights[y][x]-heights[ny][nx])<=k:
-                            q.append(ny, nx)
+                            q.append((ny, nx))
                             visited[ny][nx] = 1
             return False
-
-       left, right = 0, max([max(height[i]) for i in range(col)])
-       ans = right
-       while left<right:
-           mid = (left+right)//2
-           if bfs(mid):
-               ans = mid
-               left = mid+1
-            else:
+        
+        left, right = 0, max([max(heights[i]) for i in range(row)])
+        ans = right
+        while left<=right:
+            mid = (left+right)//2
+            if bfs(mid):
+                ans = mid
                 right = mid-1
+            else:
+                left = mid+1
         return ans
