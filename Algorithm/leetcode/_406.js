a12925c8bb0198756c92dd54abbee18f75571348
용우 남 <oponeuser7@gmail.com>
2022-06-29 22:24:55 +0900

    leetcode daily solve 406-2

--- Algorithm/leetcode/_406.js
+++ Algorithm/leetcode/_406.js
@@ -15,6 +15,26 @@
 
     people.sort(logic);
 
+    const check = (people, cur) => {
+      let count = 0;
+      for(let i=0; i<cur; i++) {
+        if(people[i][0]>=people[cur][0]) count++;
+      }
+      return count===people[cur][1];
+    }
+
+    let temp = null;
+    let cur = null;
+    for(let i=0; i<people.length; i++) {
+      cur = i;
+      while(!check(people, cur)) {
+        temp = people[cur-1];
+        people[cur-1] = people[cur];
+        people[cur] = temp;
+        cur--;
+      }
+    }
+
     return people;
 };
 
